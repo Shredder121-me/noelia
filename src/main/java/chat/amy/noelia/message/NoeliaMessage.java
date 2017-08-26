f80b81904651a368f438fba9829dd8c887b83a7f
Amy Null <amy@willnot.date>
2017-08-25 23:43:31 -0400

    Initial HeartbeatService and CommandPredicate

--- src/main/java/chat/amy/noelia/message/NoeliaMessage.java
+++ src/main/java/chat/amy/noelia/message/NoeliaMessage.java
@@ -1,7 +1,8 @@
 package chat.amy.noelia.message;
 
 import com.google.gson.JsonElement;
-import lombok.Value;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 /**
  * TODO: Document
@@ -9,10 +10,21 @@ import lombok.Value;
  * @author amy
  * @since 8/22/17.
  */
-@Value
+@Data
 @SuppressWarnings("WeakerAccess")
+@RequiredArgsConstructor
 public class NoeliaMessage {
+    /**
+     * The name of the service the message came from
+     */
     private final String source;
+    /**
+     * "Topic" of the message, to be parsed by <code>check()</code> predicates.
+     */
     private final String topic;
+    /**
+     * "Body" of the message. Should be handled by <code>accept()</code>or
+     * functions.
+     */
     private final JsonElement data;
 }
